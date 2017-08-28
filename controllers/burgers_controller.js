2d3f955ac2ed6f3690a70f4aae57ecab604c6724
Javier Avitia <Javier.A.Avitia@gmail.com>
2017-08-27 20:52:55 -0700

    Completed functionality.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,58 +6,70 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 //functions
-var burgerThisName = function(ingredientsArr){
-  var burgerName = "";
-  for (var i = 0; i < ingredientsArr.length; i++) {
-    burgerName+=ingredientsArr[i] + " ";
-  }
-  return burgerName.trim();
+var burgerThisName = function(ingredientsArr) {
+    //console.log(ingredientsArr);
+    var size = ingredientsArr[0] + " ";
+    var protein = ingredientsArr[1] + " ";
+    var toppingsArr = ingredientsArr[2];
+    var extra = ingredientsArr[3];
+    var burgerName = protein + size;
+    var check = Array.isArray(toppingsArr);
+    if (check) {
+        for (var i = 0; i < toppingsArr.length; i++) {
+            burgerName += toppingsArr[i] + " ";
+        }
+    } else {
+        burgerName += toppingsArr + " ";
+    }
+    burgerName += extra + " Burger";
+
+    return burgerName.trim();
 };
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
 router.post("/", function(req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    burgerThisName(req.body.ingredient), req.body.devoured
-  ], function() {
-    res.redirect("/");
-  });
+    burger.create([
+        "name", "devoured"
+    ], [
+        burgerThisName([req.body.size, req.body.protein, req.body.topping, req.body.extra]), false
+    ], function() {
+        res.redirect("/");
+    });
 });
 
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+    var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+    console.log("condition", condition);
 
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect("/");
+    });
 });
 
 router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+    var condition = "id = " + req.params.id;
 
-  //console.log("id: " + req.params.id);
-  console.log(condition)
+    //console.log("id: " + req.params.id);
+    console.log(condition)
 
-  burger.delete(condition, function() {
-    res.redirect("/");
-  });
+    burger.delete(condition, function() {
+        res.redirect("/");
+    });
 
-  
+
 });
 
 
